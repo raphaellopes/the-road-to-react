a781de3f56133afb9b5f8f9391788cd4042013fb
Raphael Lopes <ph.campanario07@gmail.com>
2021-04-14 17:05:47 -0300

    create basic example with stories, filter and list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
+const stories = [
+  {
+    title: 'React',
+    url: 'http://reactjs.org',
+    author: 'Jordan Walke',
+    numComments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'http://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    numComments: 2,
+    points: 5,
+    objectID: 1
+  },
+];
+
+// components
+const Search = ({ search, onSearch }) => (
+  <>
+    <label htmlFor="search">Search:</label>
+    <input
+      id="search"
+      type="text"
+      value={search}
+      onChange={onSearch}
+    />
+  </>
+);
+
+const List = ({ list }) =>
+  list.map(({ objectID, url, title, author, numComments, points }) => (
+    <div key={objectID}>
+      <span>
+        <a href={url}>{title}</a>
+      </span>
+      <span>{author}</span>
+      <span>{numComments}</span>
+      <span>{points}</span>
+    </div>
+  )
+);
+
+
+// root component
 function App() {
+  const [searchTerm, setSearchTerm] = useState('React');
+  const searchedStories = stories.filter(({ title }) =>
+    title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const handleSearch = e => {
+    setSearchTerm(e.currentTarget.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello rasta</p>
+
+      <Search search={searchTerm} onSearch={handleSearch} />
+      <hr />
+      <List list={searchedStories} />
+
     </div>
   );
 }
