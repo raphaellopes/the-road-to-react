4cc9c575e349e3455c2a424ed763388c5965e3a8
Raphael Lopes <ph.campanario07@gmail.com>
2021-04-19 15:40:11 -0300

    add axios library and replace native fetch with axios to request data from API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect, useReducer, useCallback } from 'react';
+import axios from 'axios';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -97,12 +98,12 @@ const App = () => {
 
   const handleFetchStories = useCallback(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
-    fetch(url)
-      .then(response => response.json())
+    axios
+      .get(url)
       .then(result => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits
+          payload: result.data.hits
         });
       })
       .catch(() => {
